72c7abe76b3fc26a4a43bf26967a005cf1058b61
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2025-10-23 22:18:01 +0300

    homework page

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,15 +1,25 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import css from "./NoteDetailsClient.module.css";
 
-type Props = {
-  params: Promise<{ id: string }>;
-};
+interface NoteDetailsClientProps {
+  id: string;
+}
 
-const NoteDetailsClient = async ({ params }: Props) => {
-  const { id } = await params;
-  const { title, content, createdAt } = await fetchNoteById(id);
-  const normaliseData = new Date(createdAt);
-  console.log(normaliseData);
+const NoteDetailsClient = ({ id }: NoteDetailsClientProps) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  if (isLoading) return <p>Завантаження...</p>;
+  if (isError || !data) return <p>Помилка при завантаженні нотатки</p>;
+
+  const { title, content, createdAt } = data;
+  const date = new Date(createdAt).toLocaleString();
+
   return (
     <div className={css.container}>
       <div className={css.item}>
@@ -17,7 +27,7 @@ const NoteDetailsClient = async ({ params }: Props) => {
           <h2>{title}</h2>
         </div>
         <p className={css.content}>{content}</p>
-        <p className={css.date}>{normaliseData.toLocaleString()}</p>
+        <p className={css.date}>{date}</p>
       </div>
     </div>
   );
